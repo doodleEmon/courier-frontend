e5186fae110223cf9ecca252b87a07b79e9b0bd5
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-08-27 00:12:52 +0600

    index.ts updated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,13 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+interface ImportMetaEnv {
+  readonly NEXT_PUBLIC_API_URL: string;
+}
+
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
+export const BASE_URL = `${import.meta.env.NEXT_PUBLIC_API_URL}`
 export const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 export const phoneRegex = /^(\+88)?01[3-9]\d{8}$/
